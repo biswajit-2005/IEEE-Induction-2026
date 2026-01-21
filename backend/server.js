617bd1ca5e592f1c777b5df60b0bf2a02fdd07d1
Biswajit Baliarsingh <biswajitbaliarsingh777@gmail.com>
2026-01-21 19:33:49 +0530

    add health route

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
+//health check
+app.get("/", (req, res) => res.send("Server is running"));
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   connectDB();
